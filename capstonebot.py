ed8162b0d592a6886fd3939928ca4fe3de9863ea
Jared <jared.welch1@gmail.com>
2017-04-20 11:54:19 -0500

    update active script

--- capstonebot.py
+++ capstonebot.py
@@ -4,14 +4,15 @@ from slackclient import SlackClient
 
 
 # starterbot's ID as an environment variable
-BOT_ID = os.environ.get("BOT_ID")
-
+BOT_ID = os.environ.get("bot_id")
+print(BOT_ID)
+print(os.environ.get('token'))
 # constants
 AT_BOT = "<@" + BOT_ID +">"
 EXAMPLE_COMMAND = "do"
 
 # instantiate Slack & Twilio clients
-slack_client = SlackClient(os.environ.get('bot_token'))
+slack_client = SlackClient(os.environ.get('token'))
 
 
 def handle_command(command, channel):
--- print_id.py
+++ print_id.py
@@ -3,7 +3,7 @@ from slackclient import SlackClient
 
 BOT_NAME = 'capstone_bot'
 
-slack_client = SlackClient(os.environ.get('bot_token'))
+slack_client = SlackClient(os.environ.get('token'))
 
 if __name__ == "__main__":
 	api_call = slack_client.api_call("users.list")
