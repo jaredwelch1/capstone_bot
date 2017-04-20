5ad8e65b2c44a87daaf2b4f1af6d2271fa818789
Jared <jared.welch1@gmail.com>
2017-04-20 08:46:05 -0500

    first commit

---
+++ print_id.py
@@ -0,0 +1,16 @@
+import os
+from slackclient import SlackClient
+
+BOT_NAME = 'capstone_bot'
+
+slack_client = SlackClient(os.environ.get('bot_token'))
+
+if __name__ == "__main__":
+	api_call = slack_client.api_call("users.list")
+	if api_call.get('ok'):
+		# get list of all the users to find bot
+		users = api_call.get('members')
+		for user in users:
+			if 'name' in user and user.get('name') == BOT_NAME:
+				print("Found bot, ID is" + user.get('id'))
+
